d8183eaa828670c571ce70f056241cf05400ff16
Maryna Bobryk <marynabobryk@MacBook-Pro-von-Maryna.local>
2025-02-14 17:34:46 +0100

    update-task-2

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -8,7 +8,6 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const refs = {
   btn: document.querySelector('[data-start]'),
-  timer: document.querySelector('.timer'),
   days: document.querySelector('[data-days]'),
   hours: document.querySelector('[data-hours]'),
   minutes: document.querySelector('[data-minutes]'),
@@ -17,18 +16,25 @@ const refs = {
 };
 
 //!==================================================
-
+let userDate = null;
+refs.btn.disabled = true;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+  // onReady(selectedDates, dateStr, instance) {
+  //   instance.calendarContainer.classList.add('custom-flatpickr');
+  // },
+  onReady(selectedDates, dateStr, instance) {
+    instance.calendarContainer.style.backgroundColor = '#f0f0f0';
+  },
   onClose(selectedDates) {
-    const userSelectedDay = selectedDates[0];
-    if (userSelectedDay < new Date()) {
+    userDate = selectedDates[0];
+    if (userDate < new Date()) {
       iziToast.error({
-        title: 'Error',
-        message: 'Illegal operation',
+        title: 'Oops...',
+        message: 'the day is in the past',
         iconColor: '#fff',
         titleColor: '#fff',
         messageColor: '#fff',
@@ -36,11 +42,81 @@ const options = {
         position: 'topRight',
         transitionIn: 'flipInX',
       });
+      refs.btn.disabled = true;
+    } else {
+      refs.btn.disabled = false;
     }
   },
 };
 
-const calender = flatpickr('#datetime-picker', options);
+const calender = flatpickr(refs.input, options);
+
+//!==================================================
+const countdownTimer = {
+  intervalID: null,
+
+  start() {
+    this.intervalClear();
+
+    refs.btn.disabled = true;
+    refs.input.disabled = true;
+
+    this.intervalID = setInterval(() => {
+      const timeLeft = this.updateTimer();
+      this.displayTimeLeft(timeLeft);
+      if (timeLeft <= 0) {
+        clearInterval(this.intervalID);
+        iziToast.success({
+          message: 'Time is up!',
+          iconColor: '#fff',
+          titleColor: '#fff',
+          messageColor: '#fff',
+          backgroundColor: '#94D5DB',
+          position: 'topRight',
+          transitionIn: 'flipInX',
+        });
+      }
+    }, 1000);
+    iziToast.info({
+      message: 'Timer started!',
+      iconColor: '#fff',
+      titleColor: '#fff',
+      messageColor: '#fff',
+      backgroundColor: '#89C757',
+      position: 'topRight',
+      transitionIn: 'flipInX',
+    });
+  },
+
+  updateTimer() {
+    const currentTime = new Date();
+    const diff = userDate - currentTime;
+    const time = convertMs(diff);
+    return time;
+  },
+
+  displayTimeLeft(time) {
+    refs.days.textContent = this.addLeadingZero(time.days);
+    refs.hours.textContent = this.addLeadingZero(time.hours);
+    refs.minutes.textContent = this.addLeadingZero(time.minutes);
+    refs.seconds.textContent = this.addLeadingZero(time.seconds);
+  },
+
+  addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  },
+
+  intervalClear() {
+    if (this.intervalID) {
+      clearInterval(this.intervalID);
+    }
+    this.intervalID = null;
+  },
+};
+
+refs.btn.addEventListener('click', () => {
+  countdownTimer.start();
+});
 
 //!==================================================
 
