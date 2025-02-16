2e1f46b9322b98dfa94a891d11ccbf6e93e3f95d
Maryna Bobryk <marynabobryk@MacBookPro.fritz.box>
2025-02-16 14:11:09 +0100

    update task-2-js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -16,22 +16,38 @@ const refs = {
 };
 
 //!==================================================
-let userDate = null;
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+let userSelectedDate = null;
 refs.btn.disabled = true;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  // onReady(selectedDates, dateStr, instance) {
-  //   instance.calendarContainer.classList.add('custom-flatpickr');
-  // },
   onReady(selectedDates, dateStr, instance) {
     instance.calendarContainer.style.backgroundColor = '#f0f0f0';
   },
   onClose(selectedDates) {
-    userDate = selectedDates[0];
-    if (userDate < new Date()) {
+    userSelectedDate = selectedDates[0];
+    if (userSelectedDate < new Date()) {
       iziToast.error({
         title: 'Oops...',
         message: 'the day is in the past',
@@ -51,21 +67,25 @@ const options = {
 
 const calender = flatpickr(refs.input, options);
 
-//!==================================================
 const countdownTimer = {
   intervalID: null,
 
   start() {
-    this.intervalClear();
+    clearInterval(this.intervalID);
 
     refs.btn.disabled = true;
     refs.input.disabled = true;
 
     this.intervalID = setInterval(() => {
-      const timeLeft = this.updateTimer();
-      this.displayTimeLeft(timeLeft);
-      if (timeLeft <= 0) {
+      const currentTime = new Date();
+      const diff = userSelectedDate - currentTime;
+
+      if (diff <= 0) {
         clearInterval(this.intervalID);
+        diff = 0;
+        refs.btn.disabled = true;
+        refs.input.disabled = false;
+
         iziToast.success({
           message: 'Time is up!',
           iconColor: '#fff',
@@ -76,6 +96,7 @@ const countdownTimer = {
           transitionIn: 'flipInX',
         });
       }
+      this.displayTimeLeft(diff);
     }, 1000);
     iziToast.info({
       message: 'Timer started!',
@@ -88,57 +109,19 @@ const countdownTimer = {
     });
   },
 
-  updateTimer() {
-    const currentTime = new Date();
-    const diff = userDate - currentTime;
-    const time = convertMs(diff);
-    return time;
-  },
-
-  displayTimeLeft(time) {
-    refs.days.textContent = this.addLeadingZero(time.days);
-    refs.hours.textContent = this.addLeadingZero(time.hours);
-    refs.minutes.textContent = this.addLeadingZero(time.minutes);
-    refs.seconds.textContent = this.addLeadingZero(time.seconds);
+  displayTimeLeft(diff) {
+    const { days, hours, minutes, seconds } = convertMs(diff);
+    refs.days.textContent = this.addLeadingZero(days);
+    refs.hours.textContent = this.addLeadingZero(hours);
+    refs.minutes.textContent = this.addLeadingZero(minutes);
+    refs.seconds.textContent = this.addLeadingZero(seconds);
   },
 
   addLeadingZero(value) {
     return String(value).padStart(2, '0');
-  },
-
-  intervalClear() {
-    if (this.intervalID) {
-      clearInterval(this.intervalID);
-    }
-    this.intervalID = null;
   },
 };
 
 refs.btn.addEventListener('click', () => {
   countdownTimer.start();
 });
-
-//!==================================================
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
